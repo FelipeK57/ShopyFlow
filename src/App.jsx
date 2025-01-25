124629f83882c9523b75dce9cf2a91a8c31eb3e1
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2025-01-25 10:32:25 -0500

    add TailwindCSS and HeroUI integration; remove App.css and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@heroui/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div className="bg-gray-100 h-screen flex flex-col gap-5 items-center justify-center">
+      <h1 className="font-bold text-2xl">React + TailwindCSS + HeroUI</h1>
+      <Button size="lg" color="primary" className="font-semibold">
+        Button
+      </Button>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App.jsx";
+import { HeroUIProvider } from "@heroui/react";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <HeroUIProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </HeroUIProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
